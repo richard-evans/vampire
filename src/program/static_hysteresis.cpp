3606f5a1fcd8d278255cc8bcbb793bab1d7b1dd3
Richard Evans <richard.evans@york.ac.uk>
2011-03-10 15:57:25 +0000

    Implemented new integrators in static_hysteresis() program and code tidy

--- src/program/static_hysteresis.cpp
+++ src/program/static_hysteresis.cpp
@@ -1,3 +1,24 @@
+///
+/// @file
+/// @brief Contains the Static Hysteresis program
+///
+/// @details Performs a field loop to determine field dependent magnetic behaviour
+///
+/// @section License
+/// Use of this code, either in source or compiled form, is subject to license from the authors.
+/// Copyright \htmlonly &copy \endhtmlonly Richard Evans, 2009-2010. All Rights Reserved.
+///
+/// @section info File Information
+/// @author  Richard Evans, richard.evans@york.ac.uk
+/// @version 1.1
+/// @date    10/03/2011
+/// @internal
+///	Created:		05/02/2011
+///	Revision:	10/03/2011
+///=====================================================================================
+///
+
+// Vampire Header files
 #include "atoms.hpp"
 #include "errors.hpp"
 #include "material.hpp"
@@ -8,23 +29,13 @@
 #include "vio.hpp"
 #include "vmpi.hpp"
 
+// Standard Libraries
 #include <iostream>
 #include <cmath>
 
 namespace program{
 	
 /// @brief Function to calculate a static hysteresis loop
-///
-/// @callgraph
-/// @callergraph
-///
-/// @details Consists of a sequence of sub-calculations of fixed temperature. The system is initialised 
-/// accoring to the input flag - either randomly or ordered.For the ordered case the temperature sequence
-/// increases from zero, for the random case the temperature decreases from the maximum temperature. After
-/// initialisation the sytem is equilibrated for sim::equilibration timesteps.
-///
-/// @section notes Implementation Notes
-/// Capable of hot>cold or cold>hot calculation. 
 ///
 /// @section License
 /// Use of this code, either in source or compiled form, is subject to license from the authors.
@@ -33,9 +44,8 @@ namespace program{
 /// @section Information
 /// @author  Richard Evans, rfle500@york.ac.uk
 /// @version 1.0
-/// @date    28/01/2010
+/// @date    10/03/2011
 ///
-/// @param[in] init Determines whether the system is initialised randomly (0) or ordered (1)
 /// @return EXIT_SUCCESS
 /// 
 /// @internal
@@ -44,64 +54,41 @@ namespace program{
 ///=====================================================================================
 ///
 int static_hysteresis(){
+	
 	// check calling of routine if error checking is activated
 	if(err::check==true){std::cout << "program::static_hysteresis has been called" << std::endl;}
-	
-	std::cout << "--------------------------------------------------------------------------------" << std::endl;
-	std::cout << " Running Static Hysteresis Loop Program" << std::endl;
-	std::cout << "--------------------------------------------------------------------------------" << std::endl;
-	
-
-	// Initialise spins to near-ordered state
-	for(int atom =0;atom<atoms::num_atoms;atom++){
-			atoms::x_spin_array[atom]=0.0+0.05*mtrandom::grnd()-0.1;
-			atoms::y_spin_array[atom]=0.0;
-			atoms::z_spin_array[atom]=1.0;
-	}
 	
 	// Disable temperature as this will prevent convergence
 	sim::temperature = 0.0;
 	sim::hamiltonian_simulation_flags[3] = 0;	// Thermal
+
 	// Also set up high alpha to increase convergence rate
 	for(int mat=0;mat<mp::num_materials;mat++){
 		mp::material[mat].alpha=4.0;
 	}
-	// Set H vector
-	sim::H_vec[0]=0.01;
-	sim::H_vec[1]=0.0;
-	sim::H_vec[2]=0.9999;
-	
-	double mod_H=1.0/sqrt(sim::H_vec[0]*sim::H_vec[0]+sim::H_vec[1]*sim::H_vec[1]+sim::H_vec[2]*sim::H_vec[2]);
 
+	// Ensure H vector is unit length
+	double mod_H=1.0/sqrt(sim::H_vec[0]*sim::H_vec[0]+sim::H_vec[1]*sim::H_vec[1]+sim::H_vec[2]*sim::H_vec[2]);
 	sim::H_vec[0]*=mod_H;
 	sim::H_vec[1]*=mod_H;
 	sim::H_vec[2]*=mod_H;
 	
-	// Estimate FM Corecivity
+	// Estimate Material Coercivities
 	double Hc = 2.0*mp::material[0].Ku1_SI/mp::material[0].mu_s_SI;
-	//std::cout << std::cout.flags() << std::endl;
-	//std::cout.unsetf(std::ios_base::floatfield);
-	//std::cout << std::cout.flags() << std::endl;
 	
 	std::cout << "\tEstimated Coercivity for FM is: " << Hc << " Tesla" << std::endl;
 	std::cout << "\tmu_s: " << mp::material[0].mu_s_SI << std::endl;
 	std::cout << "\tKu  : " << mp::material[0].Ku1_SI << std::endl;
 	
 	// Equilibrate system in strong positive field
-	sim::H_applied=5.0;
+	sim::H_applied=sim::Hmax;
 	sim::LLG(sim::equilibration_time);
-	
-	std::cout << "\t------------------------------------------------------------------------" << std::endl;
-	std::cout << "\tEquilibration Complete" << std::endl;
-	std::cout << "\t------------------------------------------------------------------------" << std::endl;
-	
-	// Output initial spin configuration
-	vout::pov_file();
-	
+		
 	// Setup min and max fields and increment (mT)
 	int iHmax=iround(double(sim::Hmax)*1.0E3);
 	int iHmin=iround(double(sim::Hmin)*1.0E3);
 	int iHinc=iround(double(sim::Hinc)*1.0E3);
+
 	// Perform Field Loop
 	for(int parity=-1;parity<2;parity+=2){
 		for(int H=iHmin;H<=iHmax;H+=iHinc){
@@ -109,17 +96,12 @@ int static_hysteresis(){
 			// Set applied field (Tesla)
 			sim::H_applied=double(H)*double(parity)*1.0e-3;
 			
-			//sim::partial_time=1;
 			// Simulate system
-			for(sim::time=0;sim::time<sim::total_time;sim::time+=sim::partial_time){
-				
-				// Calculate LLG
-				sim::LLG_relax(sim::partial_time);
-				
-
-				//std::cout << atoms::x_spin_array[0] << "\t" << atoms::y_spin_array[0] << "\t" << atoms::z_spin_array[0] << std::endl;
+			while(sim::time<sim::loop_time){
 			
-				//std::cin.get();
+				// Integrate system
+				sim::integrate(sim::partial_time);
+				
 				double torque=stats::max_torque();
 				if((torque<1.0e-5) && (sim::time>100)){
 					break;
@@ -130,7 +112,6 @@ int static_hysteresis(){
 			// Calculate mag_m, mag
 			stats::mag_m();
 			
-			//std::cout << sim::time << "\t" << sim::H_applied << "\t" << atoms::x_spin_array[0] << "\t" << atoms::y_spin_array[0] << "\t" << atoms::z_spin_array[0] << "\t" << stats::max_torque() << std::endl;
 			// Output to screen and file after each field
 			if(vmpi::my_rank==0){
 				std::cout << "\t" << sim::time << "\t" << sim::H_applied << "\t" << stats::total_mag_norm[0];
@@ -143,7 +124,7 @@ int static_hysteresis(){
 			}
 			
 		} // End of field loop
-} // End of parity loop
+	} // End of parity loop
 	return EXIT_SUCCESS;
 }
 
