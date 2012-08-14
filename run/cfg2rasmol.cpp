f429a082ae865aaa8cd1529138d99771fe90a990
Richard Evans <richard.evans@york.ac.uk>
2012-08-14 20:45:08 +0100

    Removed superfluous output to screen and check for existing coordinate file.

--- run/cfg2rasmol.cpp
+++ run/cfg2rasmol.cpp
@@ -16,18 +16,17 @@
 
 int main(){
 	
-	//std::vector <int> mat(0);
-	//std::vector <int> cat(0); 
-	//std::vector <double> cx(0);
-	//std::vector <double> cy(0);
-	//std::vector <double> cz(0);
-	//std::vector <std::string> type(0);
 	std::vector <std::string> filenames(0);
 	
 	// open coordinate file
 	std::ifstream coord_file;
 	coord_file.open("atoms-coords.cfg");
-
+	
+	// check for open file
+	if(!coord_file.is_open()){
+		std::cerr << "Error! Coordinate file atoms-coords.cfg cannot be opened. Exiting" << std::endl;
+	}
+	
 	// read in file header
 	std::string dummy;
 	
@@ -77,14 +76,6 @@ int main(){
 	//std::cout << dummy << std::endl;	
 	n_local_atoms=atoi(dummy.c_str());
 	
-	// resize arrays
-	//std::vector <int> mat.resize(n_atoms);
-	//std::vector <int> cat.resize(n_atoms); 
-	//std::vector <double> cx.resize(n_atoms);
-	//std::vector <double> cy.resize(n_atoms);
-	//std::vector <double> cz.resize(n_atoms);
-	//std::vector <std::string> type.resize(n_atoms);
-	
 	// Open output file
 	std::ofstream outfile;
 	outfile.open("crystal.xyz");
@@ -95,11 +86,9 @@ int main(){
 	double cx,cy,cz;
 	int mat, cat;
 	
-	std::cout << "here1" << std::endl;
 	std::string atom_type;
-	std::cout << "here2" << std::endl;
 	
-	std::cout << "Outputting data to rasmol file" << std::endl;
+	//std::cout << "Outputting data to rasmol file" << std::endl;
 	
 	// finish reading master file coordinates
 	for(int i=0;i<n_local_atoms;i++){
@@ -131,61 +120,5 @@ int main(){
 	
 	// finished
 	return 0;
-	//std::cout << n_atoms << "\t" << n_files << std::endl;
-	/*
-			// Set local output filename
-		std::stringstream file_sstr;
-		file_sstr << "atoms-coords";
-		// Set CPUID on non-root process
-		if(vmpi::my_rank!=0){
-			file_sstr << "-" << std::setfill('0') << std::setw(5) << vmpi::my_rank;
-		}
-		file_sstr << ".cfg";
-		std::string cfg_file = file_sstr.str();
-		const char* cfg_filec = cfg_file.c_str();
 
-		// Declare and open output file
-		std::ofstream cfg_file_ofstr;
-		cfg_file_ofstr.open (cfg_filec);
-		
-		// Output masterfile header on root process
-		if(vmpi::my_rank==0){
-			// Get system date
-			char *asctime( const struct tm *time_ptr );
-
-			cfg_file_ofstr << "#------------------------------------------------------"<< std::endl;
-			cfg_file_ofstr << "# Atomistic coordinates configuration file for zspin"<< std::endl;
-			cfg_file_ofstr << "#------------------------------------------------------"<< std::endl;
-			cfg_file_ofstr << "# Date: "<< asctime << std::endl;
-			cfg_file_ofstr << "#------------------------------------------------------"<< std::endl;
-			cfg_file_ofstr << "Number of atoms: "<< vout::total_output_atoms << std::endl;
-			cfg_file_ofstr << "#------------------------------------------------------" << std::endl;
-			cfg_file_ofstr << "Number of spin files: " << vmpi::num_processors-1 << std::endl;
-			for(int p=1;p<vmpi::num_processors;p++){
-				std::stringstream cfg_sstr;
-				cfg_sstr << "atoms-coords-" << std::setfill('0') << std::setw(5) << p << ".cfg";
-				cfg_file_ofstr << cfg_sstr.str() << "\"" << std::endl;
-			}
-			cfg_file_ofstr << "#------------------------------------------------------"<< std::endl;
-		}
-
-		// Everyone now outputs their atom list
-		cfg_file_ofstr << vout::local_output_atom_list.size() << std::endl;
-	  	for(int i=0; i<vout::local_output_atom_list.size(); i++){
-			const int atom = vout::local_output_atom_list[i];
-			cfg_file_ofstr << atoms::type_array[atom] << "\t" << atoms::category_array[atom] << "\t" << 
-			atoms::x_coord_array[atom] << "\t" << atoms::y_coord_array[atom] << "\t" << atoms::z_coord_array[atom] << "\t" << 
-			mp::material[atoms::type_array[atom]].element << std::endl;
-		}
-	
-		cfg_file_ofstr.close();*/
-	
-	// resize arrays
-	
-	// read in process 0 data
-	
-	// open and read in other process data
-	
-	// open and output crystal.xyz
-	
 }
