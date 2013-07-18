584835f099e7e7d1135fa9a1c33c663888abb7b9
Richard Evans <richard.evans@york.ac.uk>
2013-07-18 17:09:00 +0100

    Removed dependency of generated core-shell structure on material order in input file.

--- src/create/cs_particle_shapes.cpp
+++ src/create/cs_particle_shapes.cpp
@@ -46,8 +46,20 @@
 #include "vmath.hpp"
 #include <cmath>
 #include <cstdlib>
+#include <list>
 #include <string>
 #include <iostream>
+
+struct core_radius_t{
+   int mat;
+   double radius;
+};
+
+// comparison function for reverse order sorting
+bool compare_radius(core_radius_t first,core_radius_t second){
+   if(first.radius<second.radius) return false;
+   else return true;
+}
 
 namespace cs{
 
@@ -82,13 +94,25 @@ int cylinder(double particle_origin[],std::vector<cs::catom_t> & catom_array, co
 	// Loop over all atoms and mark atoms in sphere
 	//-----------------------------------------------
 	const int num_atoms = catom_array.size();
-	
+
+   // determine order for core-shell particles
+   std::list<core_radius_t> material_order(0);
+   for(int mat=0;mat<mp::num_materials;mat++){
+      core_radius_t tmp;
+      tmp.mat=mat;
+      tmp.radius=mp::material[mat].core_shell_size;
+      material_order.push_back(tmp);
+   }
+   // sort by increasing radius
+   material_order.sort(compare_radius);
+
  	for(int atom=0;atom<num_atoms;atom++){
 		double range_squared = 	(catom_array[atom].x-particle_origin[0])*(catom_array[atom].x-particle_origin[0]) + 
 										(catom_array[atom].y-particle_origin[1])*(catom_array[atom].y-particle_origin[1]);
 		if(mp::material[catom_array[atom].material].core_shell_size>0.0){
-			// Reverse Loop over materials
-			for(int mat=mp::num_materials-1;mat>-1;mat--){
+         // Iterate over materials
+         for(std::list<core_radius_t>::iterator it = material_order.begin(); it !=  material_order.end(); it++){
+            int mat = (it)->mat;
 				double my_radius = mp::material[mat].core_shell_size;
 				double max_range = my_radius*my_radius*particle_radius_squared;
 				double maxz=mp::material[mat].max*cs::system_dimensions[2];
@@ -149,14 +173,26 @@ void ellipsoid(double particle_origin[],std::vector<cs::catom_t> & catom_array, 
    // Loop over all atoms and mark atoms in sphere
    const int num_atoms = catom_array.size();
 
+   // determine order for core-shell particles
+   std::list<core_radius_t> material_order(0);
+   for(int mat=0;mat<mp::num_materials;mat++){
+      core_radius_t tmp;
+      tmp.mat=mat;
+      tmp.radius=mp::material[mat].core_shell_size;
+      material_order.push_back(tmp);
+   }
+   // sort by increasing radius
+   material_order.sort(compare_radius);
+
    for(int atom=0;atom<num_atoms;atom++){
       const double range_x_sq = (catom_array[atom].x-particle_origin[0])*(catom_array[atom].x-particle_origin[0]);
       const double range_y_sq = (catom_array[atom].y-particle_origin[1])*(catom_array[atom].y-particle_origin[1]);
       const double range_z_sq = (catom_array[atom].z-particle_origin[2])*(catom_array[atom].z-particle_origin[2]);
 
       if(mp::material[catom_array[atom].material].core_shell_size>0.0){
-         // Reverse Loop over materials
-         for(int mat=mp::num_materials-1;mat>-1;mat--){
+         // Iterate over materials
+         for(std::list<core_radius_t>::iterator it = material_order.begin(); it !=  material_order.end(); it++){
+            int mat = (it)->mat;
             const double my_radius_sq = mp::material[mat].core_shell_size*mp::material[mat].core_shell_size;
             double maxz=mp::material[mat].max*cs::system_dimensions[2];
             double minz=mp::material[mat].min*cs::system_dimensions[2];
@@ -204,14 +240,26 @@ int sphere(double particle_origin[],std::vector<cs::catom_t> & catom_array, cons
 	
 	// Loop over all atoms and mark atoms in sphere
 	const int num_atoms = catom_array.size();
-	
+
+   // determine order for core-shell particles
+   std::list<core_radius_t> material_order(0);
+   for(int mat=0;mat<mp::num_materials;mat++){
+      core_radius_t tmp;
+      tmp.mat=mat;
+      tmp.radius=mp::material[mat].core_shell_size;
+      material_order.push_back(tmp);
+   }
+   // sort by increasing radius
+   material_order.sort(compare_radius);
+
  	for(int atom=0;atom<num_atoms;atom++){
 		double range_squared = (catom_array[atom].x-particle_origin[0])*(catom_array[atom].x-particle_origin[0]) + 
 							 (catom_array[atom].y-particle_origin[1])*(catom_array[atom].y-particle_origin[1]) +
 							 (catom_array[atom].z-particle_origin[2])*(catom_array[atom].z-particle_origin[2]);
 		if(mp::material[catom_array[atom].material].core_shell_size>0.0){
-			// Reverse Loop over materials
-			for(int mat=mp::num_materials-1;mat>-1;mat--){
+         // Iterate over materials
+         for(std::list<core_radius_t>::iterator it = material_order.begin(); it !=  material_order.end(); it++){
+            int mat = (it)->mat;
 				double my_radius = mp::material[mat].core_shell_size;
 				double max_range = my_radius*my_radius*particle_radius_squared;
 				double maxz=mp::material[mat].max*cs::system_dimensions[2];
@@ -262,7 +310,18 @@ int truncated_octahedron(double particle_origin[],std::vector<cs::catom_t> & cat
 	
 	// Loop over all atoms and mark atoms in truncate octahedron
 	const int num_atoms = catom_array.size();
-				  	
+
+   // determine order for core-shell particles
+   std::list<core_radius_t> material_order(0);
+   for(int mat=0;mat<mp::num_materials;mat++){
+      core_radius_t tmp;
+      tmp.mat=mat;
+      tmp.radius=mp::material[mat].core_shell_size;
+      material_order.push_back(tmp);
+   }
+   // sort by increasing radius
+   material_order.sort(compare_radius);
+
 	for(int atom=0;atom<num_atoms;atom++){
 		x_vector[0] = fabs(catom_array[atom].x-particle_origin[0]);
 		x_vector[1] = fabs(catom_array[atom].y-particle_origin[1]);
@@ -270,8 +329,9 @@ int truncated_octahedron(double particle_origin[],std::vector<cs::catom_t> & cat
 		double range = x_vector[0] + x_vector[1] + x_vector[2];
 
 		if(mp::material[catom_array[atom].material].core_shell_size>0.0){
-			// Reverse Loop over materials
-			for(int mat=mp::num_materials-1;mat>-1;mat--){
+         // Iterate over materials
+         for(std::list<core_radius_t>::iterator it = material_order.begin(); it !=  material_order.end(); it++){
+            int mat = (it)->mat;
 				double my_radius = mp::material[mat].core_shell_size;
 				double my_to_height = my_radius*to_height;
 				double my_to_length = my_radius*to_length;
@@ -312,13 +372,25 @@ int cube(double particle_origin[],std::vector<cs::catom_t> & catom_array, const 
 
 	// Loop over all atoms and mark atoms in cube
 	const int num_atoms = catom_array.size();
-	
+
+   // determine order for core-shell particles
+   std::list<core_radius_t> material_order(0);
+   for(int mat=0;mat<mp::num_materials;mat++){
+      core_radius_t tmp;
+      tmp.mat=mat;
+      tmp.radius=mp::material[mat].core_shell_size;
+      material_order.push_back(tmp);
+   }
+   // sort by increasing radius
+   material_order.sort(compare_radius);
+
  	for(int atom=0;atom<num_atoms;atom++){
 		double dx=fabs(catom_array[atom].x-particle_origin[0]);
 		double dy=fabs(catom_array[atom].y-particle_origin[1]);
 		if(mp::material[catom_array[atom].material].core_shell_size>0.0){
-			// Reverse Loop over materials
-			for(int mat=mp::num_materials-1;mat>-1;mat--){
+         // Iterate over materials
+         for(std::list<core_radius_t>::iterator it = material_order.begin(); it !=  material_order.end(); it++){
+            int mat = (it)->mat;
 				double my_length = mp::material[mat].core_shell_size;
 				double max_length = my_length*side_length;
 				double maxz=mp::material[mat].max*cs::system_dimensions[2];
@@ -352,7 +424,7 @@ int tear_drop(double particle_origin[],std::vector<cs::catom_t> & catom_array, c
 	//----------------------------------------------------------
 	// check calling of routine if error checking is activated
 	//----------------------------------------------------------
-	if(err::check==true){std::cout << "cs::cube has been called" << std::endl;}
+	if(err::check==true){std::cout << "cs::tear_drop has been called" << std::endl;}
 
 	// teapdrop dimensions
 	// 0.01242725414 = 6nm/(6nm + 6nm +500nm)
