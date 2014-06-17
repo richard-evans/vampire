2c6220a4e8e021d4685aceb3df0b534461fd6668
Richard Evans <richard.evans@york.ac.uk>
2014-06-17 23:14:38 +0100

    Added initial list of input and material file parameters for later integration with vio

---
+++ src/spintorque/interface.cpp
@@ -0,0 +1,23 @@
+
+material[1]:conductivity-spin-polarisation-constant
+material[1]:diffusion-spin-polarisation-constant
+material[1]:spin-accumulation
+material[1]:spin-diffusion-length
+
+
+         beta_cond; /// spin polarisation (conductivity)
+         beta_diff; /// spin polarisation (diffusion)
+         sa_infinity; /// intrinsic spin accumulation
+         lambda_sdl; /// spin diffusion length
+         
+         
+         
+spin-torque:current-direction = x/y/z
+spin-torque:micro-cell-size
+spin-torque:micro-cell-thickness
+spin-torque:current-density
+
+
+
+
+
