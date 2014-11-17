06b6e7412a25ab73556892e853917a3c031c17a7
Richard Evans <richard.evans@york.ac.uk>
2014-11-17 23:17:55 +0000

    Incremented major version number to 4.0 for upcoming release

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -87,7 +87,7 @@ int main(int argc, char* argv[]){
       std::cout << "                                         | |               " << std::endl;
       std::cout << "                                         |_|               " << std::endl;
       std::cout << std::endl;
-      std::cout << "                      Version 3.0.3 " << __DATE__ << " " << __TIME__ << std::endl;
+      std::cout << "                      Version 4.0.0 " << __DATE__ << " " << __TIME__ << std::endl;
       std::cout << std::endl;
 
       std::cout << "  Licensed under the GNU Public License(v2). See licence file for details." << std::endl;
