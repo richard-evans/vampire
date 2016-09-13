c6f95b0218f8a9b6a245d4374087cbbb7c468a70
Richard Evans <richard.evans@york.ac.uk>
2016-09-13 17:23:34 +0100

    Added header timing function for more accurate timing of i/o routines

---
+++ hdr/vutil.hpp
@@ -0,0 +1,48 @@
+#ifndef VUTIL_H_
+#define VUTIL_H_
+//-----------------------------------------------------------------------------
+//
+// This header file is part of the VAMPIRE open source package under the
+// GNU GPL (version 2) licence (see licence file for details).
+//
+// (c) R F L Evans 2015. All rights reserved.
+//
+//-----------------------------------------------------------------------------
+
+// System headers
+#include <chrono>
+
+// Program headers
+
+//---------------------------------------------------------------------
+// Namespace including assorted utility functions
+//---------------------------------------------------------------------
+namespace vutil{
+
+   // simple class for performing code timing
+   class vtimer_t{
+      
+   private:
+      std::chrono::high_resolution_clock::time_point start_time;
+      
+   public:
+      // start the timer
+      void start(){
+         start_time = std::chrono::high_resolution_clock::now();
+      }
+      
+      // get the elapsed time in milliseconds
+      double elapsed_time(){
+         
+         // get current time
+         std::chrono::high_resolution_clock::time_point end_time = std::chrono::high_resolution_clock::now();
+         
+         // work out elapsed time
+         return 1.e-9*double(std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count());
+         
+      }
+   };
+   
+} // end of namespace vutil
+
+#endif //VUTIL_H_
