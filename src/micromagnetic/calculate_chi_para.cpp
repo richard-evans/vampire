8d72b6449cc979d9951c26b47442133d13da3e27
Sarah <sj681@york.ac.uk>
2016-10-14 12:05:53 +0100

    Change the calculations for chi_para/chi_perp

--- src/micromagnetic/calculate_chi_para.cpp
+++ src/micromagnetic/calculate_chi_para.cpp
@@ -3,17 +3,38 @@
 
 // micromagnetic module headers
 #include "internal.hpp"
-
+#include <math.h>
 namespace micromagnetic
 {
 
    namespace internal
    {
-      std::vector<double> calculate_chi_para(int num_atoms, int num_cells, std::vector<int> cell_array, const std::vector<int> type_array, std::vector <mp::materials_t> material)
+      std::vector<double> calculate_chi_para(int num_cells, double T)
       {
-         std::vector<double>  chi(num_cells,0.0);
+         //Fit Parameter
+         double a0 = 0.8;
+         double a1 =-2.2e-07;
+         double a2 = 1.95e-13;
+         double a3 =-1.3e-17;
+         double a4 =-4e-23;
+         double a5 =-6.5076312364e-32;
+      //   double chi_0 = (a0+ a1+ a2+ a3 + a4 + a5)*9.54393845712027+0.308e-14 + 0.0011;
+         std::vector<double> chi_CGS(num_cells,0.0);
+         //double chi_SI  = 0.0;
+         std::vector<double> chi(num_cells,0.0);
+         double PI= 3.14159;
+         //double factor =  0.75947907;
+         for (int cell = 0; cell < num_cells; cell++)
+         {
+            if(T<Tc[cell]) chi_CGS[cell] =(a0/660.*Tc[cell])/(4.*PI)/(Tc[cell]-T)+a1*pow((Tc[cell]-T),1.)+ a2*pow((Tc[cell]-T),3.)+a3*pow((Tc[cell]-T),4.)+ a4*pow((Tc[cell]-T),6.)+ a5*pow((Tc[cell]-T),9.);
+            else chi_CGS[cell] = (1.1*1.4/660.*Tc[cell])/(4*PI)/(T-Tc[cell]);
 
-         return chi;
+            //chi_SI = 4*PI*chi_CGS;     // CHI_SI = 4*PI Chi_CGS
+            //chi = chi_SI*4*A_FePt*A_FePt*A_FePt/MU_S_FePt/MU_0;
+            chi[cell] = chi_CGS[cell]*9.54393845712027+0.308e-14 + 0.0011; // (Tesla)
+            chi[cell] = chi[cell];
+         }
+         return chi; // [T]
       }
    }
 }
--- src/micromagnetic/calculate_chi_perp.cpp
+++ src/micromagnetic/calculate_chi_perp.cpp
@@ -3,16 +3,45 @@
 
 // micromagnetic module headers
 #include "internal.hpp"
+#include <math.h>
+#include "material.hpp"
 
 namespace micromagnetic
 {
 
    namespace internal
    {
-      std::vector<double> calculate_chi_perp(int num_atoms, int num_cells, std::vector<int> cell_array, const std::vector<int> type_array, std::vector <mp::materials_t> material)
+      std::vector<double> calculate_chi_perp(int num_cells, double T)
       {
          std::vector<double>  chi(num_cells,0.0);
+         std::vector<double>  chi_CGS(num_cells,0.0);
 
+         //Fit Parameter
+       	double a0 = 0.00211549427182711;
+       	double a1 = 0.110224660661792;
+       	double a2 = -0.855153260915204;
+       	double a3 = 3.42088365387997;
+       	double a4 = -7.84821585896818;
+       	double a5 = 10.3247035469514;
+       	double a6 = -6.85608273303224;
+       	double a7 = 0.797453198330591;
+       	double a8 = 1.53787854178089;
+       	double a9 = -0.627128148404525;
+       	double PI= 3.14159;
+
+         double chi_0 = (a0+ a1+ a2+ a3 + a4 + a5 + a6 + a7+ a8+ a9);
+         chi_0 = chi_0*9.54393845712027;
+
+         for (int cell = 0; cell < num_cells; cell ++)
+         {
+
+            if(T<(1.065*Tc[cell])) chi_CGS[cell] = a0+ a1*pow(pow(((1.068*Tc[cell]-T)/(1.068*Tc[cell])),0.5),2.)+ a2*pow((((1.068*Tc[cell])-T)/(1.068*Tc[cell])),2.)+ a3*pow((((1.068*Tc[cell])-T)/(1.068*Tc[cell])),3.)+ a4*pow((((1.068*Tc[cell])-T)/(1.068*Tc[cell])),4.) + a5*pow((((1.068*Tc[cell])-T)/(1.068*Tc[cell])),5.) + a6*pow((((1.068*Tc[cell])-T)/(1.068*Tc[cell])),6.) + a7*pow((((1.068*Tc[cell])-T)/(1.068*Tc[cell])),7.)+ a8*pow((((1.068*Tc[cell])-T)/(1.068*Tc[cell])),8.) + a9*pow((((1.068*Tc[cell])-T)/(1.068*Tc[cell])),9.);
+            else chi_CGS[cell] = (0.8*1.4/660.*Tc[cell])/(4*PI)/(T-Tc[cell]);
+
+            chi[cell] = (chi_CGS[cell]*9.54393845712027); // (Tesla)
+            chi[cell] = chi[cell]/chi_0;
+            chi[cell] =  -ms[cell]*chi[cell]/ku[cell];
+         }
          return chi;
       }
    }
