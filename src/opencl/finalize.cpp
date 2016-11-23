7b44f50752a2acac20c330ea4466a15f88af8341
srhm501 <srhm501@york.ac.uk>
2016-11-23 22:28:09 +0000

    Free memory for Device3D objects

--- src/opencl/finalize.cpp
+++ src/opencl/finalize.cpp
@@ -1,10 +1,27 @@
 #include "vopencl.hpp"
 
+#include "data.hpp"
+
+#ifdef OPENCL
+namespace vcl = ::vopencl::internal;
+#endif
+
 namespace vopencl
 {
    void finalize(void)
    {
 #ifdef OPENCL
+
+      vcl::atoms::spin_array.free();
+      vcl::atoms::coord_array.free();
+
+      vcl::cells::coord_array.free();
+      vcl::cells::mag_array.free();
+      vcl::cells::field_array.free();
+
+      vcl::total_spin_field_array.free();
+      vcl::total_external_field_array.free();
+      vcl::dipolar_field_array.free();
 
 #endif //OPENCL      
    }
