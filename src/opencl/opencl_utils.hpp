5edc7b68b328fa1532825f6a05b53774854c85e3
srhm501 <srhm501@york.ac.uk>
2016-12-19 13:38:58 +0000

    Add some comments

--- src/opencl/opencl_utils.hpp
+++ src/opencl/opencl_utils.hpp
@@ -12,12 +12,15 @@
 
 #define UNUSED(x) (void)(x)
 
+// functions to pass given arguments to kernel k which recursively sets argument i
+// the following function is needed for when all the arguments have been set
 static void pass_args(cl::Kernel &k, unsigned i)
 {
    UNUSED(k);
    UNUSED(i);
 }
 
+// function which takes an arg from args and gives it to the kernel for arg i
 template <typename Car, typename... Cdr>
 static void pass_args(cl::Kernel &k, unsigned i, Car car, Cdr... cdr)
 {
@@ -37,18 +40,24 @@ namespace vopencl
 {
    namespace internal
    {
+      // function to return a kernel from a given file name and kernel name
+      // the opts string can be used to define things as with gcc
+      // for example "-DN" defines N (for use in #ifdef)
+      // and "-DN=10" will replace N with 10 in the kernel code
       cl::Kernel build_kernel_from_file(const std::string &file_name,
                                         const std::string &kernel_name,
                                         const cl::Context &context,
-                                        const cl::Device &device,
+                                        const cl::Device  &device,
                                         const std::string &opts="");
 
+      // function to enqueue a kernel
+      // the OpenCL C++ spec for this seems pretty fluid so it's probably better not to use it
       template <typename... Ts>
-      static void kernel_call(cl::Kernel &k,
-                              cl::CommandQueue &q,
-                              cl::NDRange gbl,
-                              cl::NDRange lcl,
-                              Ts... Args)
+      static void kernel_call(cl::Kernel &k,        /* kernel to enqueue */
+                              cl::CommandQueue &q,  /* into this queue */
+                              cl::NDRange gbl,      /* total number of work items */
+                              cl::NDRange lcl,      /* number of work items in group */
+                              Ts... Args)           /* kernel arguments */
       {
          pass_args(k, 0, Args...);
          q.enqueueNDRangeKernel(k, cl::NullRange, gbl, lcl);
