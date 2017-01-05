432f475d8bf7b073aef76778af9a42648208d6e5
srhm501 <srhm501@york.ac.uk>
2017-01-05 19:32:57 +0000

    Fix for single precision Vectors will (probably) be double, not T (float for single precision)

--- src/opencl/typedefs.hpp
+++ src/opencl/typedefs.hpp
@@ -14,101 +14,107 @@ namespace vopencl
    namespace internal
    {
 #ifdef OPENCL_DP
-      typedef cl_double vcl_real_t;
+      typedef cl_double  vcl_real_t;
+      typedef cl_double3 vcl_real_vec_t;
 #else
-      typedef cl_float vcl_real_t;
+      typedef cl_float  vcl_real_t;
+      typedef cl_float3 vcl_real_vec_t;
 #endif // OPENCL_DP
 
 
       // Class for set of three buffers, i.e. for x,y,z arrays
-      // uses non blocking reads/writes
-      template <typename T>
+      // uses non blocking reads/writes where possible
+
+      // T is standard C++ type (i.e. float)
+      // T_vec is OpenCL type (i.e. float3)
+      template <typename T, typename T_vec>
       class Buffer3D
       {
-         std::vector<cl::Buffer> buffers;
-         size_t buffer_size;
+         std::vector<cl::Buffer> buffer;
+         size_t n_elems;
 
       public:
 
-         Buffer3D(void) noexcept : buffers(3), buffer_size(0) {}
+         Buffer3D(void) noexcept : buffer(1), n_elems(0) {}
 
          // initialize without writing, but with size
-         // e.g. for use when generating buffers on device
+         // e.g. for use when generating buffer on device
          Buffer3D(const cl::Context &c,
                   const cl_mem_flags fs,
-                  const size_t size) noexcept
-            : buffers(3), buffer_size(size)
+                  const size_t n) noexcept
+            : buffer(1), n_elems(n)
          {
-            buffers[0] = cl::Buffer(c, fs, size);
-            buffers[1] = cl::Buffer(c, fs, size);
-            buffers[2] = cl::Buffer(c, fs, size);
+            buffer[0] = cl::Buffer(c, fs, n*sizeof(T_vec));
          }
 
           // initialize with 3 vectors to write data to device
+         template <typename R>
          Buffer3D(const cl::Context &c,
                   const cl::CommandQueue &q,
                   const cl_mem_flags fs,
-                  const std::vector<T> &xs,
-                  const std::vector<T> &ys,
-                  const std::vector<T> &zs) noexcept
-            : buffers(3)
+                  const std::vector<R> &xs,
+                  const std::vector<R> &ys,
+                  const std::vector<R> &zs) noexcept
+            : buffer(1)
          {
-            buffer_size = xs.size();
+            n_elems = xs.size();
 
-            buffers[0] = cl::Buffer(c, fs, sizeof(T)*xs.size());
-            buffers[1] = cl::Buffer(c, fs, sizeof(T)*ys.size());
-            buffers[2] = cl::Buffer(c, fs, sizeof(T)*zs.size());
+            buffer[0] = cl::Buffer(c, fs, sizeof(T_vec)*xs.size());
 
-            q.enqueueWriteBuffer(buffers[0], CL_FALSE, 0, sizeof(T)*xs.size(), &xs[0]);
-            q.enqueueWriteBuffer(buffers[1], CL_FALSE, 0, sizeof(T)*ys.size(), &ys[0]);
-            q.enqueueWriteBuffer(buffers[2], CL_FALSE, 0, sizeof(T)*zs.size(), &zs[0]);
+            std::vector<T_vec> buff(n_elems);
+            for (size_t i=0; i<n_elems; ++i)
+            {
+               buff[i] = {T(xs[i]), T(ys[i]), T(zs[i])};
+            }
+
+            q.enqueueWriteBuffer(buffer[0], CL_FALSE, 0, sizeof(T_vec)*n_elems, &buff[0]);
          }
 
          // reads data from device, assumes host vectors already have enough capacity
+         template <typename R>
          void copy_to_host(const cl::CommandQueue &q,
-                           std::vector<T> &xs,
-                           std::vector<T> &ys,
-                           std::vector<T> &zs) const noexcept
+                           std::vector<R> &xs,
+                           std::vector<R> &ys,
+                           std::vector<R> &zs) const noexcept
          {
-            q.enqueueReadBuffer(buffers[0], CL_FALSE, 0, sizeof(T)*xs.size(), &xs[0]);
-            q.enqueueReadBuffer(buffers[1], CL_FALSE, 0, sizeof(T)*ys.size(), &ys[0]);
-            q.enqueueReadBuffer(buffers[2], CL_FALSE, 0, sizeof(T)*zs.size(), &zs[0]);
+            std::vector<T_vec> buff(n_elems);
+            q.enqueueReadBuffer(buffer[0], CL_FALSE, 0, sizeof(T_vec)*n_elems, &buff[0]);
+
+            q.finish();
+
+            for (size_t i=0; i<n_elems; ++i)
+            {
+               xs[i] = R(buff[i].s[0]);
+               ys[i] = R(buff[i].s[1]);
+               zs[i] = R(buff[i].s[2]);
+            }
          }
 
-         // copies buffers to dst buffers on device
+         // copies buffer to dst buffer on device
          void copy_to_dev(const cl::CommandQueue &q,
                           Buffer3D &dst,
                           const size_t amount_to_copy) const noexcept
          {
-            q.enqueueCopyBuffer(buffers[0], dst.x(), 0, 0, amount_to_copy);
-            q.enqueueCopyBuffer(buffers[1], dst.y(), 0, 0, amount_to_copy);
-            q.enqueueCopyBuffer(buffers[2], dst.z(), 0, 0, amount_to_copy);
+            q.enqueueCopyBuffer(buffer[0], dst.buffer[0], 0, 0, amount_to_copy);
          }
 
-         void zero_buffers(void)
+         void zero_buffer(void) noexcept
          {
 #ifdef CL_API_SUFFIX__VERSION_1_2
-            const T zero = 0;
-            for (const cl::Buffer &buffer : buffers)
-               vcl::queue.enqueueFillBuffer(buffer, &zero, sizeof T, buffer_size);
+            const T_vec zero = {0};
+            vcl::queue.enqueueFillBuffer(buffer[0], &zero, sizeof T_vec, n_elems);
 #else
-            const std::vector<T> zeros(buffer_size, 0);
-            for (const cl::Buffer &buffer : buffers)
-               vcl::queue.enqueueWriteBuffer(buffer, CL_FALSE, 0, sizeof(T) * buffer_size, &zeros[0]);
+            const std::vector<T_vec> zeros(n_elems, {0});
+            vcl::queue.enqueueWriteBuffer(buffer[0], CL_FALSE, 0, sizeof(T_vec) * n_elems, &zeros[0]);
 #endif // CL_API_SUFFIX__VERSION_1_2
 
             vcl::queue.finish();
          }
 
-         // access each buffer, e.g. for passing to kernel
-         cl::Buffer &x(void) noexcept { return buffers[0]; }
-         cl::Buffer &y(void) noexcept { return buffers[1]; }
-         cl::Buffer &z(void) noexcept { return buffers[2]; }
-
          // release memory by swapping with empty vector
          void free(void) noexcept
          {
-            std::vector<cl::Buffer>().swap(buffers);
+            std::vector<cl::Buffer>().swap(buffer);
          }
       };
 
