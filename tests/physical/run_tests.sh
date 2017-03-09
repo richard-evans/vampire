6912b6a5a0245982195c91cc497fff5b541ac45e
srhm501 <srhm501@york.ac.uk>
2017-03-09 15:42:36 +0000

    Rewrite testing script, makes it easier to add more tests/options

--- tests/physical/run_tests.sh
+++ tests/physical/run_tests.sh
@@ -5,10 +5,30 @@ readonly green='\033[0;32m'
 readonly red='\033[0;31m'
 readonly nc='\033[0m'
 
+function help_message {
+    echo "Script to perform physical tests on VAMPIRE."
+    echo "Author: S R H Morris"
+    echo
+    echo "Options:"
+    echo " --help (-h): Prints this message."
+    echo
+    echo " --test (-t): Performs the test number supplied."
+    echo "              If this option is not given all tests are performed."
+    echo "              Valid numbers are 1 - Tests applied field and integrator."
+    echo "                                2 - Tests anisotropy and thermal field."
+    echo "                                3 - Tests exchange."
+}
+
+function cleanup {
+    # restore old files
+    mv output.bak output 2>/dev/null
+    mv input.bak input   2>/dev/null
+    mv Co.mat.bak Co.mat 2>/dev/null
+}
 
 function is_within_tolerance {
-    local readonly tolerance=0.000001
     local readonly error=$(echo $1 | sed 's/[eE]+*/\*10\^/')
+    local readonly tolerance=$(echo $2 | sed 's/[eE]+*/\*10\^/')
 
     if (( $(echo $error '<' $tolerance | bc -l) )); then
 	echo -e "${green}passed${nc} (maximum error $1)"
@@ -17,43 +37,88 @@ function is_within_tolerance {
     fi
 }
 
+function applied_field {
+    echo -n "Testing applied magnetic field..........."
 
-# backup input and material files
-mv input input.bak
-mv Co.mat Co.mat.bak
+    dir=tests/physical/AppliedField
+
+    cp $dir/input input
+    cp $dir/Co.mat Co.mat
+
+    ./vampire &>/dev/null
+
+    # check vampire output against analytic results
+    $dir/applied_field_errors.py > applied_field_errors.dat
+    max_error=$(grep "# maximum error = " applied_field_errors.dat | awk '{print $5}')
+    is_within_tolerance $max_error 1e-6
+}
+
+function thermal {
+    echo -n "Testing thermal effects.................."
+
+    dir=tests/physical/Thermal
+
+    cp $dir/input input
+    cp $dir/Co.mat Co.mat
+
+    ./vampire &>/dev/null
+    max_error=$($dir/boltzmann_distribution.py)
+
+    is_within_tolerance $max_error 0.01
+}
+
+function mag_vs_t {
+    echo -n "Testing magnetisation with temperature..."
+
+    dir=tests/physical/MvT
+
+    cp $dir/input input
+    cp $dir/Co.mat Co.mat
+
+    ./vampire &>/dev/null
+    max_error=$($dir/mvt.py)
+
+    is_within_tolerance $max_error 0.01
+}
+
+function perform_test {
+
+    case $1 in
+        1)
+            applied_field
+            ;;
+        2)
+            thermal
+            ;;
+        3)
+            mag_vs_t
+            ;;
+        *)
+            echo -e "${red}Error: unknown test number $1. See --help for details."
+            ;;
+    esac
+}
 
 
+trap cleanup EXIT INT
 
-#### Test One ####
-echo -n "Testing applied magnetic field....."
+# backup old files
+mv output output.bak 2>/dev/null
+mv input input.bak   2>/dev/null
+mv Co.mat Co.mat.bak 2>/dev/null
 
-dir=tests/physical/AppliedField
+while [[ $# -gt 0 ]]
+do
+    key="$1"
 
-# use single spin in magnetic field input and material files
-cp $dir/input input
-cp $dir/Co.mat Co.mat
-
-./vampire &>/dev/null
-
-# check vampire output against analytic results
-$dir/applied_field_errors.py > applied_field_errors.dat
-max_error=$(grep "# maximum error = " applied_field_errors.dat | awk '{print $5}')
-is_within_tolerance $max_error
-#### End Test One ####
-
-#### Test Two ####
-echo -n "Testing thermal effects............"
-
-dir=tests/physical/Thermal
-
-cp $dir/input input
-cp $dir/Co.mat Co.mat
-
-#./vampire &>/dev/null
-
-is_within_tolerance 0.01
-
-
-# restore old input and material file
-mv input.bak input
-mv Co.mat.bak Co.mat
+    case $key in
+        -h|--help)
+            help_message
+            exit
+            ;;
+        -t|--test)
+            perform_test $2
+            shift 2
+            ;;
+    esac
+done
