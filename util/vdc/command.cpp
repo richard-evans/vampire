6b9353f1ed54ebe16646d3de019ce68620dc3c17
dm858 <dm858@york.ac.uk>
2017-10-22 21:41:56 +0100

    Finished writing command line parser for 3D vectors

--- util/vdc/command.cpp
+++ util/vdc/command.cpp
@@ -21,7 +21,13 @@
 
 namespace vdc{
 
-   void command( int argc, char* argv[] ){
+// forward function declarations
+void extract( std::string string_z, std::vector<double> vector_z );
+
+//------------------------------------------------------------------------------
+// Command line parsing function
+//------------------------------------------------------------------------------
+void command( int argc, char* argv[] ){
 // Command line options for utility to be implemented:
 //    --xyz - generate xyz file
 //    --povray - generate povray files
@@ -35,92 +41,104 @@ namespace vdc{
 //    --objects = spins, cones, spheres, cubes
 //    --slice = x,x,y,y,z,z
 //    --multiscale = gradient, material, region
-      std::string string_z = "(0.0,0.0,1.0)"
-      std::string string_x = "(1.0,0.0,0.0)"
+   std::string string_z = "(0.0,0.0,1.0)"
+   std::string string_x = "(1.0,0.0,0.0)"
 
-      std::vector<double> vector_z = {0.0,0.0,1.0};
-      std::vector<double> vector_x = {1.0,0.0,0.0};
+   std::vector<double> vector_z = {0.0,0.0,1.0};
+   std::vector<double> vector_x = {1.0,0.0,0.0};
 
-      for (int arg = 1; arg < argc; arg++){
+   for (int arg = 1; arg < argc; arg++){
 
-         // read prefix
-         std::string sw=argv[arg]
-         if (sw == "--vector-z"){
+      // read prefix
+      std::string sw=argv[arg]
+      if (sw == "--vector-z"){
 
-            // check number of args not exceeded
-            if (arg+1 < argc){
-               arg++;
-               vector_z = string(argv[arg]);
-            }
-            else {
-               terminaltextcolor(RED);
-               std::cerr << "Error - expected 3 comma separated variables in brackets."
-                         << "\n" << "Check for spaces in command-line arguments"
-                         << std:endl;
-               terminaltextcolor(WHITE);
-               return EXIT_FAILURE;
-            }
-
-            // work through vector and extract values
-            extract(string_z,vector_z);
+         // check number of args not exceeded
+         if (arg+1 < argc){
+            arg++;
+            vector_z = string(argv[arg]);
+         }
+         else {
+            terminaltextcolor(RED);
+            std::cerr << "Error - expected 3 comma separated variables in brackets."
+                      << "\n" << "Check for spaces in command-line arguments"
+                      << std:endl;
+            terminaltextcolor(WHITE);
+            return EXIT_FAILURE;
          }
 
+         // work through vector and extract values
+         extract(string_z,vector_z);
       }
 
    }
 
-   void extract( std::string string_z, std::vector<double> vector_z ){
-      int marker = 0; //position in the vector string
+}
 
-      // check for opening brackets
-      if ( string_z[marker] == ( "(" || "{" ) ) {
-         //-----------------------------------------------
-         // move to next character
+//------------------------------------------------------------------------------
+// Extracts 3D vector coordinates from string: {x,y,z} or (x,y,z)
+// where x,y and z are doubles
+//------------------------------------------------------------------------------
+void extract( std::string string_z, std::vector<double> vector_z ){
+   int marker = 0; // position in the vector string
+
+   // check for opening brackets
+   if ( string_z[marker] != ( "(" || "{" ) ){
+      terminaltextcolor(RED);
+      std::cerr << "Error - brackets required around 3 comma separated values"
+                << std::endl;
+      terminaltextcolor(WHITE);
+      return EXIT_FAILURE;
+   }
+
+   // move to next character
+   marker++;
+
+   // read coordinates
+   for ( int i = 0; i < 3; i++){
+      std::string tmp_string;
+
+      // read coordinate-value
+      int j = 0;
+      while ( string_z[marker] != "," ){
+         tmp_string.resize( tmp_string.size() +1 );
+         tmp_string[j] = string_z[marker];
+
+         // move through number
          marker++;
-
-         // read coordinates
-         read_vector( vect, x, marker );
-         read_vector( vect, y, marker );
-         read_vector( vect, z, marker );
-
-         // normalise (x,y,z)
-         double length;
-         length = std:sqrt( x*x + y*y + z*z );
-         x = x/length;
-         y = y/length;
-         z = z/length;
-
-
+         j++;
       }
-      else {
+
+      vector_z[i] = std::stod(tmp_string);
+
+      // skip comma, check for closing brackets
+      if ( string_z[marker] == "," ){
+         marker++;
+      }
+      else if ( (string_z[marker] != ( ")" || "}" )) && ( i == 2 ){
          terminaltextcolor(RED);
          std::cerr << "Error - brackets required around 3 comma separated values"
                    << std::endl;
          terminaltextcolor(WHITE);
          return EXIT_FAILURE;
       }
-   }
-
-   void read_vector( std::string vect, double& coordinate, int& marker ){
-      std::string tmp_string;
-      int i = 0;
-
-      // read coordinate-value
-      while ( vect[marker] != "," ) {
-         tmp_string.resize( tmp_string.size() +1 );
-         tmp_string[i] = vect[marker];
-         marker++;
-         i++;
+      else if ( (string_z[marker] == ( ")" || "}" )) && ( i != 2 ){
+         terminaltextcolor(RED)
+         std::cerr << "Error - three coordinates required"
+                   << std::endl;
+         terminaltextcolor(WHITE);
+         return EXIT_FAILURE;
       }
 
-      // move marker off comma
-      marker++;
-
-      // convert from string to double
-      coordinate = std::stod(tmp_string)
    }
 
+   // normalise vector_z
+   double length;
+   length = std:sqrt( vector_z[0]*vector_z[0] + vector_z[1]*vector_z[1] + vector_z[2]*vector_z[2] );
+   vector_z[0] = vector_z[0]/length;
+   vector_z[1] = vector_z[1]/length;
+   vector_z[2] = vector_z[2]/length;
 
-
+}
 
 } // end of namespace vdc
