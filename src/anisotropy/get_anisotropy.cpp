06fa5fa19d970135a754bb5b4762c1bdcd9f72e3
Richard Evans <richard.evans@york.ac.uk>
2017-10-24 00:23:17 +0100

    Added functions to extract anisotropy constants from module

--- src/anisotropy/get_anisotropy.cpp
+++ src/anisotropy/get_anisotropy.cpp
@@ -27,4 +27,47 @@ namespace anisotropy{
       return internal::mp[material].ku2;
    }
 
+   //--------------------------------------------------------------------------------
+   // Function to get second order uniaxial anisotropy constant for a given material
+   //--------------------------------------------------------------------------------
+   double get_ku2(const int material){
+      return internal::mp[material].ku2;
+   }
+
+   //--------------------------------------------------------------------------------
+   // Function to get fourth order uniaxial anisotropy constant for a given material
+   //--------------------------------------------------------------------------------
+   double get_ku4(const int material){
+      return internal::mp[material].ku4;
+   }
+
+   //--------------------------------------------------------------------------------
+   // Function to get sixth order uniaxial anisotropy constant for a given material
+   //--------------------------------------------------------------------------------
+   double get_ku6(const int material){
+      return internal::mp[material].ku6;
+   }
+
+   //--------------------------------------------------------------------------------
+   // Function to get fourth order cubic anisotropy constant for a given material
+   //--------------------------------------------------------------------------------
+   double get_kc4(const int material){
+      return internal::mp[material].kc4;
+   }
+
+   //--------------------------------------------------------------------------------
+   // Function to get sixth order cubic anisotropy constant for a given material
+   //--------------------------------------------------------------------------------
+   double get_kc6(const int material){
+      return internal::mp[material].kc6;
+   }
+
+   //--------------------------------------------------------------------------------
+   // Function to get unit vector defining axis for uniaxial anisotropy for a material
+   //--------------------------------------------------------------------------------
+   std::vector<double> get_ku_vector(const int material){
+      return internal::mp[material].ku_vector;
+   }
+
+
 } // end of anisotropy namespace
