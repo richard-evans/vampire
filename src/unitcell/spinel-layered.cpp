1487dbf4974c8c6294a74f9236a1942a6512aeaa
Andrea Meo <andreameo90@gmail.com>
2022-02-13 15:52:05 +0100

    Add correct file creator of "spinel-layered.cpp"

--- src/unitcell/spinel-layered.cpp
+++ src/unitcell/spinel-layered.cpp
@@ -3,7 +3,7 @@
 //   This file is part of the VAMPIRE open source package under the
 //   Free BSD licence (see licence file for details).
 //
-//   (c) Richard F L Evans 2019. All rights reserved.
+//   (c) Andrea Meo 2022. All rights reserved.
 //
 //   Email: richard.evans@york.ac.uk
 //
