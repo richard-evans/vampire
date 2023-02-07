24be4df3b019183f336566e37bd39257e960ceab
Jackson <jackson.ross@york.ac.uk>
2023-02-07 09:39:09 +0100

    rutile magnetic unitcell

--- src/unitcell/internal.hpp
+++ src/unitcell/internal.hpp
@@ -74,6 +74,7 @@ namespace unitcell{
       void build_honeycomb_beta(unitcell::unit_cell_t& unit_cell);
       void build_kagome(unitcell::unit_cell_t& unit_cell);
       void build_rock_salt(unitcell::unit_cell_t& unit_cell);
+      void build_rutile(unitcell::unit_cell_t& unit_cell);
       void build_heusler(unitcell::unit_cell_t& unit_cell);
       void build_spinel(unitcell::unit_cell_t& unit_cell);
       void build_spinel_layered(unitcell::unit_cell_t& unit_cell);
